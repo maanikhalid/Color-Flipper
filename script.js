8e241392c79729dfd7e79822dbf8f2d83bfcaf6a
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-15 17:16:13 +0000

    Finished
    This is now my 4th Javascript test.
    
    This was the first time I completely done a project using Javascript without the help of a tutorial.
    
    Yes! I google some issues i had. But thats the skill of being a programmer right?..... right🥺?
    
    Anyhoo... on to the next project.

--- script.js
+++ script.js
@@ -1,89 +1,71 @@
 /* Lets & Consts*/
 let newColorValue = ""
 let color = [
-    mainColor = newColorValue,
-    colorName = "Scarlet Red",
-    rgbaValue = "12,12,12,1",
+    mainColor = "",
+    colorName = "Random Color Generator",
+    rgbaValue = mainColor,
     hexValue = "#ff4400"
 ]
-let newColorEl = document.getElementById("newColor")
-let genRedValue = ""
-let genGreenValue = ""
-let genBlueValue = ""
-
-
-function randomRGBvalue(){
-    var returnColor = Math.floor(Math.random() * (255 - 1 + 1) + 1)
-    return returnColor
-}
-
-function genNewColor(){
-genRedValue = randomRGBvalue()
-genGreenValue = randomRGBvalue()
-genBlueValue = randomRGBvalue()
-
-newColorValue = genRedValue + "," + genGreenValue + "," +genBlueValue
-
-}
-
-newColorEl.addEventListener("click", genNewColor());
-
-console.log(newColorValue)
-
-///* Generating a new color */
-//function generateColor(min, max){
-//    return Math.floor(Math.random() * (max - min + 1) + min)
-//}
-//
-//
-//
-///* Listen event for clicks */
-//newColorEl.addEventListener("click", function(){
-//    console.log("You've clicked the button")
-//    genBlueValue = generateColor(1,255)
-//    genRedValue = generateColor(1,255)
-//    genGreenValue = generateColor(1,255)
-//    
-//    return newColorValue = genRedValue + "," + genGreenValue + "," +genBlueValue
-//})
-//
-
-
-
-
-
-
-/* Storing new color */
-color[0] = newColorValue
-color[1] = "Purple Rain"
-color[2] = color[0]
-color[3] = "#00000"
-
 
 /*Getting DOM's */
-const mainColorEl = document.documentElement.style.setProperty('--main-color', color[0])
-const mainColorInverseEl = document.documentElement.style.setProperty('--main-color-inverse', color[0])
-const colorNameEl = document.getElementById("colorNameEl")
-const rgbaValueEl = document.getElementById("rgbaValueEl")
-const hexValueEl = document.getElementById("hexValueEl")
+let mainColorEl = document.documentElement.style.setProperty('--main-color', color[0])
+let mainColorInverseEl = document.documentElement.style.setProperty('--main-color-inverse', color[0])
+let colorNameEl = document.getElementById("colorNameEl")
+let rgbaValueEl = document.getElementById("rgbaValueEl")
+let hexValueEl = document.getElementById("hexValueEl")
+const newColorEl = document.querySelector("#newColor")
 
+/* Choose new color on page load */
+window.onload = function (){
+    generateNewColor()
+}
 
-/* Connecting new colors to DOM */
-/*mainColorEl.textContent = color[0]*/
-colorNameEl.textContent = color[1]
-rgbaValueEl.textContent = "RGB: " + color[2]
-hexValueEl.textContent = "HEX: " + color[3]
+/* Generate a random RGB Value */
+function randomRGB(newValue){
+    
+    newValue = Math.floor(Math.random() * (255 - 1 + 1) + 1) +","+ Math.floor(Math.random() * (255 - 1 + 1) + 1)+","+ Math.floor(Math.random() * (255 - 1 + 1) + 1)
+   
+    return newValue
+}
 
-/* Local Storage */
-function setColorsLocal() {
+/* Update with new color */
+function generateNewColor(){
+    color[0] = randomRGB(color[0])
+    color[2] = color[0]
+    
+    /* Convert RGB to HEX */
+    const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+    
+    color[3] = rgb2hex('rgb(' + color[0] + ')')
+
+    color.push(color[0])
+    color.push(color[2])
+    color.push(color[3])
+    
+    rgbaValueEl.textContent = "RGB: " + color[2]
+    hexValueEl.textContent = "HEX: " + color[3]
+
+    mainColorEl = document.documentElement.style.setProperty('--main-color', color[0])
+    mainColorInverseEl = document.documentElement.style.setProperty('--main-color-inverse', color[0])
+    
+    
     localStorage.clear()
     localStorage.setItem("Main Color", color[0])
     localStorage.setItem("Color Name", color[1])
-    localStorage.setItem("RGBA Value", color[2])
+    localStorage.setItem("RGB Value", color[0])
     localStorage.setItem("HEX Value", color[3])
+
 }
 
+rgbaValueEl.addEventListener("dblclick", function(){
+    console.log("Ready to copy")
+        rgbaValueEl.innerHTML = "RGB: " + color[2] + "<span>✅</span>"
+        navigator.clipboard.writeText(color[2]);
+})
 
-/* Store to Local Storage - Must run after colors have been set */
-setColorsLocal()
 
+hexValueEl.addEventListener("dblclick", function(){
+    console.log("Ready to copy")
+        hexValueEl.innerHTML = "HEX: " + color[3] + " <span>✅</span>"
+        navigator.clipboard.writeText(color[3]);
+})
